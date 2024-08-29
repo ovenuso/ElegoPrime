a75e462ccbcd3d8a45cd809674dbc4cc32368026
ElegoPrime <mapi2004g@gmail.com>
2024-08-29 13:56:33 -0500

    fix env

--- ElegoSolutionsLLC-main/elegoweb/js/index.js
+++ ElegoSolutionsLLC-main/elegoweb/js/index.js
@@ -114,6 +114,7 @@ nuevoDocumento.save()
 // Comentario de env
 console.log('MONGODB_URI:', process.env.MONGODB_URI);
 console.log('EMAIL_USER:', process.env.EMAIL_USER);
+console.log('EMAIL_PASS:', process.env.EMAIL_PASS);
 console.log('PORT:', process.env.PORT);
 
 
